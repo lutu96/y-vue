c36a0665aab081a11460d7d4be720bbab25a6dca
caiyb <caiyb@sh.woowis.com>
2022-03-22 18:38:40 +0800

    解决parcel编译问题

--- src/button.vue
+++ src/button.vue
@@ -15,7 +15,7 @@ export default {
             type: String
         },
         loading: {
-            type: String,
+            type: Boolean,
             default: false
         },
         iconPosition: {

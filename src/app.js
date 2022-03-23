85629b2a3a077df50d041345b1e60c3088679763
caiyb <caiyb@sh.woowis.com>
2022-03-23 18:06:08 +0800

    使用chai为button组件进行单元测试

--- src/app.js
+++ src/app.js
@@ -13,3 +13,86 @@ new Vue ({
         loading3: true
     }
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+const expect = chai.expect
+{
+    let Button  = Vue.extend(YButton)
+    const vm = new Button({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    const property = vm.$el.querySelector('use').getAttribute('xlink:href')
+    expect(property).to.eq('#y-setting')
+    vm.$destroy()
+}
+
+{
+    let Button  = Vue.extend(YButton)
+    const vm = new Button({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    })
+    vm.$mount()
+    const property = vm.$el.querySelector('use').getAttribute('xlink:href')
+    expect(property).to.eq('#y-loading')
+    vm.$destroy()
+}
+
+{   
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    let Button  = Vue.extend(YButton)
+    const vm = new Button({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'left'
+        }
+    })
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    const  { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq('1')
+    div.remove()
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{   
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    let Button  = Vue.extend(YButton)
+    const vm = new Button({
+        propsData: {
+            icon: 'setting',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    const  { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq('2')
+    div.remove()
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    let Button  = Vue.extend(YButton)
+    const vm = new Button({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    const spy = chai.spy(function () {})
+    vm.$on('click', spy)
+    vm.$el.click()
+    expect(spy).to.have.been.called()
+    vm.$destroy()
+}

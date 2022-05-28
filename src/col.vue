6880b11da72417419a238c3e69cdb0c1014ad87f
caiyb <caiyb@sh.woowis.com>
2022-05-28 23:15:22 +0800

    col && row

---
+++ src/col.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="col" :class="colClass" :style="colStyle">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'y-col',
+    props: {
+        span: {
+            type: String | Number,
+        },
+        offset: {
+            type: String | Number,
+        }
+
+    },
+    data () {
+        return {
+            gutter: 0
+        }
+    },
+    computed: {
+        colClass () {
+            let { span, offset } = this
+            return [
+                span && `col-${span}`,
+                offset && `offset-${offset}`
+            ]
+        },
+        colStyle () {
+            return {
+                paddingLeft: this.gutter / 2 + 'px',
+                paddingRight: this.gutter / 2 + 'px'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+     $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+}
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="row" :style="rowStyle" :class="rowClass">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'y-row',
+    props: {
+        gutter: {
+            type: Number | String,
+            default: 0
+        },
+        align: {
+            type: String,
+            validator (value) {
+            return ['left', 'right', 'center'].includes(value)
+            }
+        }
+    },
+    computed: {
+        rowStyle () {
+            let { gutter } = this
+            console.log(gutter)
+            return {
+                marginLeft: -gutter / 2 + 'px',
+                marginRight: -gutter/ 2 + 'px'
+            }
+        },
+        rowClass () {
+            let {align} = this
+            return [align && `align-${align}`]
+        }
+    },
+    mounted () {
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .row {
+        display: flex;
+        &.align-left {
+            justify-content: flex-start;
+        }
+        &.align-right {
+        justify-content: flex-end;
+        }
+        &.align-center {
+        justify-content: center;
+        }
+    }
+    
+</style>

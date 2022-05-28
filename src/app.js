89dad6cd5a886d0a73cc4b355b3dd6fee6123233
caiyb <caiyb@sh.woowis.com>
2022-05-29 00:01:45 +0800

    布局组件完成

--- src/app.js
+++ src/app.js
@@ -5,12 +5,22 @@ import YButtonGroup from './buttonGroup.vue'
 import YInput from './input.vue'
 import YRow from './row.vue'
 import YCol from './col.vue'
+import YLayout from './layout.vue'
+import YHeader from './header.vue'
+import YContent from './content.vue'
+import YSider from './sider.vue'
+import YFooter from './footer.vue'
 Vue.component('y-icon', YIcon)
 Vue.component('y-button', YButton)
 Vue.component('y-button-group',YButtonGroup)
 Vue.component('y-input', YInput)
 Vue.component('y-row', YRow)
 Vue.component('y-col', YCol)
+Vue.component('y-layout', YLayout)
+Vue.component('y-header', YHeader)
+Vue.component('y-content', YContent)
+Vue.component('y-sider', YSider)
+Vue.component('y-footer', YFooter)
 new Vue ({
     el: '#app',
     data: {
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'y-sider'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sider {
+  }
+</style>
